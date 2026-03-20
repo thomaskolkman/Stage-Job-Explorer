0bc4bc746fabf2485b2f617d6a381c4d3e04cd0e
gheerschap <g.heerschap@blauwolk.com>
2026-03-20 10:05:05 +0100

    styling updated

--- resources/views/components/app-links.blade.php
+++ resources/views/components/app-links.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block px-4 py-4 mt-2 text-sm font-semibold text-white bg-gradient-to-br from-indigo-950 to-purple-950 rounded-lg dark:bg-gradient-to-br from-indigo-900 to-purple-900 dark:hover:bg-gradient-to-br from-indigo-900 to-purple-900 dark:focus:bg-gradient-to-br from-indigo-900 to-purple-900 focus:outline-none focus:shadow-outline'
-            : 'block px-4 py-4 mt-2 text-sm font-semibold text-white rounded-lg dark:hover:bg-gradient-to-br from-indigo-900 to-purple-900 dark:focus:bg-gradient-to-br from-indigo-900 to-purple-900 focus:outline-none focus:shadow-outline';
+    ? 'block px-4 py-4 mt-2 text-sm font-semibold text-white bg-gradient-to-br from-indigo-700 to-indigo-900 hover:from-indigo-600 hover:to-indigo-800 rounded-lg focus:outline-none focus:shadow-outline'
+    : 'block px-4 py-4 mt-2 text-sm font-semibold text-white rounded-lg hover:bg-gradient-to-br hover:from-indigo-600 hover:to-indigo-800 focus:outline-none focus:shadow-outline';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
         <div class="flex-col w-full md:flex md:flex-row md:min-h-screen">
             <div @click.away="open = false" class="flex flex-col flex-shrink-0 w-full text-gray-700 md:w-64 dark:text-gray-200 dark:bg-gradient-to-br from-indigo-950 to-purple-950" x-data="{ open: false }">
                 <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4">
-                    <a :href="route('dashboard')">
+                    <a href="/dashboard" class="w-48">
                         <img src="{{ asset('images/stage-job-explorer-logo.png') }}" alt="Stage Job Explorer">
                     </a>
                     <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
@@ -67,18 +67,18 @@
                 <nav :class="{'block': open, 'hidden': !open}" class="flex-grow px-4 pb-4 md:block md:pb-0 md:overflow-y-auto">
                         <x-app-links :href="route('dashboard')" :active="request()->routeIs('dashboard')">Dashboard</x-app-links>
                     <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-4 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark:bg-transparent dark:focus:text-white dark:hover:text-white dark:focus:bg-gradient-to-br from-indigo-950 to-purple-950 dark:hover:bg-gradient-to-br from-indigo-900 to-purple-900 md:block focus:outline-none focus:shadow-outline">
+                        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-4 mt-2 text-sm font-semibold text-left text-white rounded-lg dark:focus:bg-gradient-to-br from-indigo-700 to-indigo-900 dark:hover:bg-gradient-to-br from-indigo-600 to-indigo-800 md:block focus:outline-none focus:shadow-outline">
                             <span>{{ Auth::user()->name }}</span>
                             <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
                         <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
                             <div class="px-2 py-2 rounded-md shadow dark:bg-gradient-to-br from-indigo-950 to-purple-950">
-                                <x-dropdown-link class="block px-4 py-2 mt-2 text-white text-sm font-semibold rounded-lg hover:bg-gradient-to-br from-indigo-900 to-purple-900 focus:bg-gradient-to-br from-indigo-900 to-purple-900 md:mt-0 focus:outline-none focus:shadow-outline" :href="route('profile.edit')">
+                                <x-dropdown-link class="block px-4 py-2 mt-2 text-white text-sm font-semibold rounded-lg hover:bg-gradient-to-br from-indigo-600 to-indigo-800 focus:bg-gradient-to-br from-indigo-700 to-indigo-900 md:mt-0 focus:outline-none focus:shadow-outline" :href="route('profile.edit')">
                                     {{ __('Edit') }}
                                 </x-dropdown-link>
                                 <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-                                    <x-dropdown-link class="block px-4 py-2 mt-2 text-white text-sm font-semibold rounded-lg hover:bg-gradient-to-br from-indigo-900 to-purple-900 focus:bg-gradient-to-br from-indigo-900 to-purple-900 md:mt-0 focus:outline-none focus:shadow-outline" :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                                    <x-dropdown-link class="block px-4 py-2 mt-2 text-white text-sm font-semibold rounded-lg hover:bg-gradient-to-br from-indigo-600 to-indigo-800 focus:bg-gradient-to-br from-indigo-700 to-indigo-900 md:mt-0 focus:outline-none focus:shadow-outline" :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                                         {{ __('Log Out') }}
                                     </x-dropdown-link>
                                 </form>
@@ -87,7 +87,7 @@
                     </div>
                 </nav>
             </div>
-            <div class="flex w-full bg-gradient-to-br from-indigo-950 to-purple-900">
+            <div class="flex flex-col w-full bg-gradient-to-br from-indigo-950 to-purple-900">
                 {{ $slot }}
             </div>
         </div>
