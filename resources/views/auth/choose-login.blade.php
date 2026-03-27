34b9b9f4b913341c7129686533473a2bbcecd503
gheerschap <g.heerschap@blauwolk.com>
2026-03-27 13:30:18 +0100

    white text

--- resources/views/auth/choose-login.blade.php
+++ resources/views/auth/choose-login.blade.php
@@ -11,7 +11,7 @@
 
             <x-login-button href="#" label="Bedrijf Login" variant="secondary" />
 
-            <div class="py-3 flex items-center text-sm text-indigo-800 before:flex-1 before:border-t before:border-indigo-800 before:me-6 after:flex-1 after:border-t after:border-indigo-800 after:ms-6">
+            <div class="py-3 flex items-center text-sm text-white before:flex-1 before:border-t before:border-white before:me-6 after:flex-1 after:border-t after:border-white after:ms-6">
                 Choose Login Type
             </div>
         
